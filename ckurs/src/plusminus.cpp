6ff22004cd31e4a505ae166f39a317fdefb03620
Raikuso <44640217+Raikuso@users.noreply.github.com>
2020-09-02 16:46:27 +0200

    Update plusminus.cpp

--- ckurs/src/plusminus.cpp
+++ ckurs/src/plusminus.cpp
@@ -22,39 +22,39 @@ int main565(){
 	cout << sum << endl;
 	cout << "-----------" << endl;
 
-	//so wie der Schüler es (eigentlich nicht) machte, (aber wir tun einfach mal so)
+	//so wie der SchÃ¼ler es (eigentlich nicht) machte, (aber wir tun einfach mal so)
 	for(int i = 0, j = MAX, sum = 0L; i <= MAX/2; sum+=(i++)+(j--));
 	cout << sum << endl;
 	cout << "-----------" << endl;
 
-	//so wie der Schüler es tatsächlich machte (die beste lösung 8D)
+	//so wie der SchÃ¼ler es tatsÃ¤chlich machte (die beste lÃ¶sung 8D)
 	cout << (MAX+1)*MAX/2 << endl;
 	cout << "-----------" << endl;
 
 
-	//körner auf einem schachbrett zusammenzählen. Körner verdoppeln sich auf jedem Feld.
+	//kÃ¶rner auf einem schachbrett zusammenzÃ¤hlen. KÃ¶rner verdoppeln sich auf jedem Feld.
 	sum = 0L;
 	for(int i = 1; i<=64; ++i){
 		sum += korn;
-		cout << "Feld: " << i << " Körner: " << korn << " Summe: " << sum << endl;
+		cout << "Feld: " << i << " KÃ¶rner: " << korn << " Summe: " << sum << endl;
 		korn = pow(2,i);
 	}
 
-	cout << "Es befinden sich " << sum << " Körner auf dem Feld.\n" << endl << "Das Gesamtgewicht beträgt: " << sum*KORN_GEWICHT << " Tonnen." << endl;
-	cout << "Es werden " << (sum*KORN_GEWICHT)/10000 << " Züge benötigt, um alle Körner abzutransportieren." << endl;
-	cout << "Die Transportationsdauer beträgt: " << (sum*KORN_GEWICHT)/10000/48 << " Tage" << endl;
+	cout << "Es befinden sich " << sum << " KÃ¶rner auf dem Feld.\n" << endl << "Das Gesamtgewicht betrÃ¤gt: " << sum*KORN_GEWICHT << " Tonnen." << endl;
+	cout << "Es werden " << (sum*KORN_GEWICHT)/10000 << " ZÃ¼ge benÃ¶tigt, um alle KÃ¶rner abzutransportieren." << endl;
+	cout << "Die Transportationsdauer betrÃ¤gt: " << (sum*KORN_GEWICHT)/10000/48 << " Tage" << endl;
 	cout << "Oder: " << (sum*KORN_GEWICHT)/10000/48/365.25 << " Jahre" << endl;
 
 /*
   	Die gleiche Aufgabe in kryptisch
-	i ist der Zähler, k sind die körner und die Variable die nach links geshiftet wird, n ist das limit, summe ist die gesamtsumme aller Körner
-	Bei jeder ausführung wird zuerst die summe mit k verodert. Als nächstes wird k um 1 nach links geshiftet um die Zahl zu verdoppeln.
-	Zuletzt wird der Zähler erhöht und die Schleife wiederholt sich bis das Limit erreicht wird.
-		1.		2.		3.		4.		5.
-  k	  	0001	0010	0100	1000	10000
-  sum 	0001	0001	0011	0111	01111
-  k^sum	0001	0011	0111	1111	11111
-  dec	1d		3d		7d		15d		31d
+	i ist der ZÃ¤hler, k sind die kÃ¶rner und die Variable die nach links geshiftet wird, n ist das limit, summe ist die gesamtsumme aller KÃ¶rner
+	Bei jeder ausfÃ¼hrung wird zuerst die summe mit k verodert. Als nÃ¤chstes wird k um 1 nach links geshiftet um die Zahl zu verdoppeln.
+	Zuletzt wird der ZÃ¤hler erhÃ¶ht und die Schleife wiederholt sich bis das Limit erreicht wird.
+		1.	2.	3.	4.	5.
+  k	  	0001	0010	0100	1000	10000		
+  sum 		0001	0001	0011	0111	01111
+  k^sum		0001	0011	0111	1111	11111
+  dec		1d		3d		7d		15d		31d
 
   */
 	for (unsigned long long i = 1, k = 1, n = 64, sum = 1; i < n; cout << (sum ^= (k <<= 1)) << " bis Feld: " << ++i << endl);
